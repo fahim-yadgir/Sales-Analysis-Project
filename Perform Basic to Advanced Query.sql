a9e07e2e1eb994dd46ef34dd67446944795cf225
Fahim <fahimyadgir07@gmail.com>
2026-07-02 23:52:27 +0530

    perform some year type quetion

--- Perform Basic to Advanced Query.sql
+++ Perform Basic to Advanced Query.sql
@@ -109,3 +109,48 @@ from sales_analysis_demo;
 
 select max(sales)as max_sales
 from sales_analysis_demo;
+
+select category,round(sum(sales),2)as total_sales
+from sales_analysis_demo
+group by category;
+
+select category,round(sum(profit),2)as total_profit
+from sales_analysis_demo
+group by category;
+
+select state,round(avg(sales),2)as avg_sales
+from sales_analysis_demo
+group by state;
+
+select region,round(sum(quantity),2)as total_quanity
+from sales_analysis_demo
+group by region;
+
+select ship_mode,round(sum(sales),2)as total_ship_mode
+from sales_analysis_demo
+group by ship_mode;
+
+select state,count(Order_ID)as total_orders
+from sales_analysis_demo
+group by state;
+
+select Customer_Name,count(order_id)as total_orders
+from sales_analysis_demo
+group by customer_name;
+
+select sub_category,round(sum(sales),2)as total_sales
+from sales_analysis_demo
+group by sub_category;
+
+select 
+	year(order_date)as order_year,
+    round(sum(sales),2)as total_Sales
+from sales_analysis_demo
+group by year(order_date)
+order by order_year;
+
+select Category,round(sum(sales),2)as sales_greater_than_50000
+from sales_analysis_demo
+group by Category
+having sum(sales)>5000
+order by Category desc;
