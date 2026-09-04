7d2cb0c98348b93050bc1eb8cbbbdd56488bf1b7
Fahim <fahimyadgir07@gmail.com>
2026-09-04 22:58:19 +0530

    Create one view for to see second class ship mode

--- Perform Basic to Advanced Query.sql
+++ Perform Basic to Advanced Query.sql
@@ -515,3 +515,11 @@ order by Order_Date asc
 )
 
 select * from `2015_to_2016`;
+
+create view second_class_mode as
+(
+select * from sales_analysis_demo
+where Ship_Mode = 'Second Class'
+)
+
+select * from second_class_mode;
